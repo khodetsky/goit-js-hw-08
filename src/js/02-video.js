c2740d488504c3b5b2916b2a0356ad16144d01a5
khodetsky <hodyaya@gmail.com>
2022-05-29 00:35:39 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import trottle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getCurrentTime = function () {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem("videoplayer-current-time", `${seconds}`)
+    });
+};
+const setCurrentTime = function () {
+    const currentTime = localStorage.getItem("videoplayer-current-time");
+    player.setCurrentTime(currentTime);
+};
+
+player.on('timeupdate', trottle(getCurrentTime, 1000));
+player.on('play', setCurrentTime);
