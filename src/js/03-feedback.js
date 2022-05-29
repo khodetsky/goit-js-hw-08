4745c95375ad5ff238c5a8a5e7ce8084f1449ab0
khodetsky <hodyaya@gmail.com>
2022-05-29 19:26:57 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import trottle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form")
+const formInput = document.querySelector(".feedback-form input");
+const formTextarea = document.querySelector(".feedback-form textarea");
+
+const formData = {};
+
+function onFormClick(evt) {
+    formData[evt.target.name] = evt.target.value;
+    const formDataJSOM = JSON.stringify(formData);
+    localStorage.setItem("feedback-form-state", formDataJSOM)
+};
+
+function populateForm() {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+    const formValues = JSON.parse(savedMessage);
+    if (savedMessage) {
+        formInput.value = formValues.email; 
+        formTextarea.value = formValues.message;  
+    }
+}
+
+form.addEventListener('input', onFormClick);
+form.addEventListener('input', populateForm)
+
+// function onFormInputClick(evt) {
+//     const email = evt.target.value;
+//     console.log(email);
+// };
+
+// function onTextareaInput(evt) {
+//     const message = evt.target.value;
+//     console.log(message);
+// };
+
+// formInput.addEventListener('input', onFormInputClick);
+// formTextarea.addEventListener('input', onTextareaInput)
+ 
